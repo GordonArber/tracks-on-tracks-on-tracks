79542e82c69f12942e02e0f6b04cd0c9b9b0ebf7
Gordon Arber <Gordon.arber@gmail.com>
2021-10-27 11:53:25 -0400

    Initial commit

---
+++ TracksOnTracksOnTracks.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Languages
+{
+    public static List<string> NewList()
+    {
+        return new List<string>();
+    }
+
+    public static List<string> GetExistingLanguages()
+    {
+        var existingList =  new List<string>()
+        {
+            "C#",
+            "Clojure",
+            "Elm",                
+        };
+        return existingList;
+    }
+
+    public static List<string> AddLanguage(List<string> languages, string language)
+    {
+        languages.Add(language);
+        return languages;
+    }
+
+    public static int CountLanguages(List<string> languages)
+    {
+        return languages.Count;
+    }
+
+    public static bool HasLanguage(List<string> languages, string language)
+    {
+        return languages.Contains(language);
+    }
+
+    public static List<string> ReverseList(List<string> languages)
+    {
+        languages.Reverse();
+        return languages;
+    }
+
+    public static bool IsExciting(List<string> languages)
+    {
+        if (languages.Count <= 0)
+            return false;
+        
+        return languages[0].Equals("C#") || languages[1].Equals("C#") && languages.Count is 2 or 3;
+    }
+
+    public static List<string> RemoveLanguage(List<string> languages, string language)
+    {
+        languages.Remove(language);
+        return languages;
+    }
+
+    public static bool IsUnique(List<string> languages)
+    {
+        return languages.Distinct().Count() == languages.Count;
+    }
+}
